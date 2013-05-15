3bb03bcddf509048c55b87605c2179d7583f56a8
Alan Gonzalez <alanboy@alanboy.net>
2013-05-15 02:42:05 -0500

    Quitando info privada del config.

--- teddy.itc.mx/www/config.php
+++ teddy.itc.mx/www/config.php
@@ -10,25 +10,21 @@
 	base de datos 
 *************************** */
 	//no cambiar el nombre de estas variables, ni el orden
-	$TEDDY_DB_USER 		= "teddy";
-	$TEDDY_DB_PASS 		= "teddy.itc.mx";
-	$TEDDY_DB_NAME 		= "teddy";
-	$TEDDY_DB_SERVER 	= "localhost";
+	$TEDDY_DB_USER 		= "";
+	$TEDDY_DB_PASS 		= "";
+	$TEDDY_DB_NAME 		= "";
+	$TEDDY_DB_SERVER 	= "";
 
 /* ***************************
 	twitter 
 *************************** */
 	//cuenta para twittear
-	$TEDDY_TWITTER_USER = "clubdeprogra";
+	$TEDDY_TWITTER_USER = "";
 	$TEDDY_TWITTER_MUTE = true;
 
 
 /* ***************************
 	google analytics 
 *************************** */
-	//id de google analytics
-	$TEDDY_GA_ID = "UA-11327997-2";
-
-	//mute para los analytics, bueno para cuando estas
-	//codificando ya que GA no le importa de donde venga
+	$TEDDY_GA_ID = "";
 	$TEDDY_GA_MUTE = true;
